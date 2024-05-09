cca7df124d774807bb4726cda0617c7e377172f8
Mdkkdjssnsb <163823860+Mdkkdjssnsb@users.noreply.github.com>
2024-05-09 17:28:36 +0530

    Update and rename index.cjs (2).txt to index.cjs

--- index.cjs
+++ index.cjs
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'System', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/api/youtube', async (req, res) => {
